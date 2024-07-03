7dc72dd6aab7a5b1b15c5e1aa7a63a35bfc02de6
Aung Ye Htet <aung.atg1738@gmail.com>
2024-07-03 22:13:14 +0630

    add: Get Route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // /users
+export class UsersController {
+  /* 
+    Get/users
+    Get/users/:id
+    Post/users
+    Patch/users/:id
+    Delete/users/:id
+  */
+
+  @Get() // Get/users
+  findAll() {
+    return [];
+  }
+
+  @Get('girlfriends') // Get/users/girlfriends
+  findAllGirlfriends() {
+    return "Bro, you're not gonna find any here.";
+  }
+
+  @Get(':id') // Get/users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+}
